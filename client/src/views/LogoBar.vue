3198f40188baf29eea76e43bd65743ee03b1d46e
WiktorRudnicki <rudnicki.w01@htlwienwest.at>
2021-10-15 15:20:52 +0200

    Logobar

--- client/src/views/LogoBar.vue
+++ client/src/views/LogoBar.vue
@@ -1,6 +1,8 @@
 <template>
     <v-app-bar class=" black" height="200px">
         <img src="../../public/logo.png" class="mx-2" style="height: 150px; margin-top: 0px"/>
+        <v-spacer/>
+        <span href="https://www.imdb.com/movies-in-theaters/">Latest releases</span>
     </v-app-bar>
 </template>
 
